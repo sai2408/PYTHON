9ab063a2f8cede16292a75eab33aee529e7e4951
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2022-03-13 12:42:14 +0530

    Add files via upload

---
+++ stack/stack.py
@@ -0,0 +1,32 @@
+#STACK IMLEMENTATION USINF ARRAYS
+'''
+Linear data structure
+elements are arranged in a sequential
+follows LIFO Mechanism
+best example is plates in a tray
+                arranging the coins
+
+PUSH - inserting the element
+POP - deleting the element from the stack
+TOP - top of the stack(Where push and pop operations can be performed)
+    if there is no elements in the stack then the value of the TOP is -1
+'''
+#conditions to be verified
+'''
+1.OVER FLOW CONDITION:-
+    When the space is not availabke for thr new element
+    Inserting element to the srtack which is having maximum elements
+    have to check at push opertion
+
+2.UNDER FLOW CONDITION:-
+    deleteing the element from the empty stack
+    have to check at pop opertion
+'''
+'''
+There are different implementations in python
+1.list
+2.collections.deque
+3.queue.LifoQueue
+'''
+
+                
